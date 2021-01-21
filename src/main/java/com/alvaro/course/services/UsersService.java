ece71c5f1a32a445bccbf5cd312cea7d9ff2013e
Álvaro Neto <alvaro_neto96@hotmail.com>
2021-01-21 12:53:28 -0300

    Service layer, component registration

---
+++ src/main/java/com/alvaro/course/services/UsersService.java
@@ -0,0 +1,26 @@
+package com.alvaro.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alvaro.course.entities.User;
+import com.alvaro.course.repositories.UserRepository;
+
+@Service
+public class UsersService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
