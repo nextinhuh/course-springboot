fc71a0791ac7e8a984bedbf2aa5170e18a3fa506
Álvaro Neto <alvaro_neto96@hotmail.com>
2021-01-25 10:06:36 -0300

    User insert

--- src/main/java/com/alvaro/course/entities/User.java
+++ src/main/java/com/alvaro/course/entities/User.java
@@ -22,7 +22,7 @@ public class User implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	private String emai;
+	private String email;
 	private String phone;
 	private String password;
 
@@ -33,11 +33,11 @@ public class User implements Serializable {
 	public User() {
 	}
 
-	public User(Long id, String name, String emai, String phone, String password) {
+	public User(Long id, String name, String email, String phone, String password) {
 		super();
 		this.id = id;
 		this.name = name;
-		this.emai = emai;
+		this.email = email;
 		this.phone = phone;
 		this.password = password;
 	}
@@ -58,12 +58,12 @@ public class User implements Serializable {
 		this.name = name;
 	}
 
-	public String getEmai() {
-		return emai;
+	public String getEmail() {
+		return email;
 	}
 
-	public void setEmai(String emai) {
-		this.emai = emai;
+	public void setEmai(String email) {
+		this.email = email;
 	}
 
 	public String getPhone() {
